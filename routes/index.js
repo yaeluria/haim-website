f13f4c48f245924c316f21763fcef1584e863774
Yael Luria <yaeluria@gmail.com>
2019-01-10 17:16:22 +0200

    make images on images index bigger, button appears on top of images on hover, create routes and templates for works and press sections.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,17 @@ router.get("/", function(req, res){
     res.render("landing");
 });
 
+router.get("/events", function(req, res){
+    res.render("events");
+});
 
+router.get("/works", function(req, res){
+    res.render("construction");
+});
+
+router.get("/press", function(req, res){
+    res.render("construction");
+});
 
 
 module.exports = router;
